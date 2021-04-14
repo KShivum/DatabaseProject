1ff279199ab5d37761100185529a8c9d00887761
Shivum Kumar <shivumkumar1@gmail.com>
2021-04-13 19:36:37 -0500

    Added a majority of the main page
    I am missing assigning employees in

--- Housekeeping/Housekeeping/Pages/Main/Index.cshtml.cs
+++ Housekeeping/Housekeeping/Pages/Main/Index.cshtml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Housekeeping.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Housekeeping.Pages.Main
 {
@@ -15,11 +18,47 @@ namespace Housekeeping.Pages.Main
         {
             db = _db;
         }
-        [BindProperty]
-        public User User { get; set; }
+        [TempData]
+        public int userId { get; set; }
+        public User LoginUser { get; set; }
+        public List<Room> Rooms {get; set;}
+        public List<User> Housekeepers { get; set; }
         public async Task OnGet(int id)
         {
-            User = await db.User.FindAsync(id);
+            LoginUser = await db.User.FindAsync(id);
+            userId = id;
+            Rooms = await db.Room.ToListAsync();
+            Housekeepers = await db.User.Where(u => u.Role.Equals("HouseKeeper")).ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostChangeStatus(int roomNo, string status, User user)
+        {
+            var roomPrev = await db.Room.FindAsync(roomNo);
+            var roomNew = roomPrev;
+            roomNew.Status = status;
+            var log = new Log();
+            var tempUserId = (int)TempData.Peek("userId");
+
+            log.RoomNo = roomNo;
+            log.Assignee = tempUserId;
+            log.AssignedEmployee = roomPrev.AssignedEmployee;
+            log.StatusChangedFrom = roomPrev.Status;
+            log.StatusChangedTo = status;
+            log.Date = DateTime.Now;
+
+            db.Room.Update(roomNew);
+            db.Log.Add(log);
+            TempData.Clear();
+            await db.SaveChangesAsync();
+            return RedirectToPage($"/Main/Index", new { id = tempUserId });
+
+
+        }
+
+        public void changeEmp(ChangeEventArgs e)
+        {
+
+        }
+
     }
 }
